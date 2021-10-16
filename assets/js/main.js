f1056a33251c1d7cb7ab2df8475b0f700aeb12d4
markosrb97 <marko.radosavljevic97@gmail.com>
2021-10-16 21:04:49 +0200

    search

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,12 @@
 const menu =  document.getElementById("menu");
+const searchBar = document.getElementsByClassName("search")[0]
 const button = document.getElementsByClassName("menu-button")[0]
-
+const srcImg = document.getElementById("src-pic")
 
 button.addEventListener("click",(e)=> {
     menu.classList.toggle("menu-active")
+})
+
+srcImg.addEventListener("click", (e)=> {
+    searchBar.classList.toggle("search-active")
 })
